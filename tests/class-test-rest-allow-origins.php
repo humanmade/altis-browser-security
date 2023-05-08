887632ba75f4e2ffc6ae9f9359113ced1f4a4e93
Jerico Aragon <jerico@gmail.com>
2023-05-08 14:25:09 +0800

    Update unit tests

--- tests/class-test-rest-allow-origins.php
+++ tests/class-test-rest-allow-origins.php
@@ -11,25 +11,40 @@ class Test_Rest_Allow_Origins extends WP_UnitTestCase {
 		parent::setUp();
 	}
 
+	function test_filter_function() {
+		$result = restrict_cors_origin( true );
+		$this->assertTrue( restrict_cors_origin( $result ) );
+	}
+
 	function test_filter_is_added() {
 		add_filter( 'altis.security.browser.rest_allow_origin', '__return_false' );
-		$this->assertTrue( has_filter( 'altis.security.browser.rest_allow_origin' ) );
+		bootstrap( array() );
+
+		$this->assertNotFalse( has_filter( 'rest_pre_dispatch', __NAMESPACE__ . '\\restrict_cors_origin' ) );
 	}
 
 	function test_dynamic_filter_disallow_local() {
 		$allow = true;
-		$origin = 'https://example.local'; 
 
 		add_filter( 'altis.security.browser.rest_allow_origin', function ( $allow, $origin ) {
-			if ( strpos( $origin, '.local' ) >= 0 ) {
+			if ( strpos( $origin, '.local' ) ) {
 				return false;
 			}
-		
-			return false;
+			return true;
 		}, 10, 2 );
 
-		$result = apply_filters( 'altis.security.browser.rest_allow_origin', $allow, $origin );
+		add_filter( 'http_origin', function( $origin ) {
+			return 'https://example.local'; 
+		} );
 
-		$this->assertFalse( $result );
+		$result = restrict_cors_origin( true );
+		$this->assertInstanceOf( 'WP_Error', $result );
+
+		add_filter( 'http_origin', function( $origin ) {
+			return 'https://example.com'; 
+		} );
+
+		$result = restrict_cors_origin( true );
+		$this->assertNotInstanceOf( 'WP_Error', $result );
 	}
 }
